8c0ec3c0bcc95e067f0b358b01ef49074d9dc72d
mithileshrane007 <mithilesh.r@infiny.in>
2017-04-19 14:29:30 +0530

    Locationmanger got location w/o presenter in splash

--- app/src/main/java/com/example/infiny/ezrent/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/infiny/ezrent/ui/splash/SplashActivity.java
@@ -1,16 +1,21 @@
 package com.example.infiny.ezrent.ui.splash;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.infiny.ezrent.R;
 import com.example.infiny.ezrent.ui.base.BaseActivity;
 import com.example.infiny.ezrent.ui.base.LocationPresenter;
 import com.yayandroid.locationmanager.configuration.Configurations;
 import com.yayandroid.locationmanager.configuration.LocationConfiguration;
+import com.yayandroid.locationmanager.constants.FailType;
+import com.yayandroid.locationmanager.constants.ProcessType;
 
 public class SplashActivity extends BaseActivity implements SplashMvpView,LocationPresenter.LocationView {
 
 
+    private LocationPresenter locationPresenter;
 
     @Override
     public LocationConfiguration getLocationConfiguration() {
@@ -21,7 +26,8 @@ public class SplashActivity extends BaseActivity implements SplashMvpView,Locati
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        locationPresenter = new LocationPresenter(this);
+        getLocation();
 
     }
 
@@ -29,8 +35,36 @@ public class SplashActivity extends BaseActivity implements SplashMvpView,Locati
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (getLocationManager().isWaitingForLocation()
+                && !getLocationManager().isAnyDialogShowing()) {
+            showLoading();
+        }
     }
 
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        dismissProgress();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        super.onLocationChanged(location);
+        locationPresenter.onLocationChanged(location);
+
+    }
+    @Override
+    public void onLocationFailed(@FailType int failType) {
+        locationPresenter.onLocationFailed(failType);
+    }
+
+    @Override
+    public void onProcessTypeChanged(@ProcessType int processType) {
+        locationPresenter.onProcessTypeChanged(processType);
+    }
 
     @Override
     public String getLocationDetails() {
@@ -39,16 +73,17 @@ public class SplashActivity extends BaseActivity implements SplashMvpView,Locati
 
     @Override
     public void setLocationDetails(String text) {
-
+        Log.d("locv",text);
     }
 
     @Override
     public void updateProgress(String text) {
+        Log.d("locv",text);
 
     }
 
     @Override
     public void dismissProgress() {
-
+        hideLoading();
     }
 }
